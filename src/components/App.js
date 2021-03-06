5988e8712a7f6a97e3a348398e95e52034b996ec
liorange0119 <orange.riisa0119@gamil.com>
2021-03-06 09:32:18 +0900

    create seName

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SignIn from './SignIn';
 
 export default () => {
-  return <div><SignIn /></div>;
+  // [状態(変数), 変数を変更するための関数(ここで名前設定)] = useState(nameの初期値);
+  const [name, setName] = useState('');
+  console.log({ name });
+  // SignInコンポーネントのblocksの１つとしてsetNameが渡ってくる
+  // compornentに値を渡す => props
+  return <SignIn setName={setName} />;
 };
