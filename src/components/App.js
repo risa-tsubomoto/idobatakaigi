7fdf6271bb422ee0283618e7657418d1eba0e53a
liorange0119 <orange.riisa0119@gamil.com>
2021-03-13 18:40:55 +0900

    create config json

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 
 import Main from './Main';
 import SignIn from './SignIn';
+// 開発用設定
+import config from '../config.json';
+console.log({ config });
 
 export default () => {
   // [状態(変数), 変数を変更するための関数(ここで名前設定)] = useState(nameの初期値);
   const [name, setName] = useState('');
-  console.log({ name });
   
   // nameが設定されている場合、Main compornentを表示。
-  if(name === ''){
+  if(config.signInEnabled && name === ''){
     // SignInコンポーネントのpropsの１つとしてsetNameが渡ってくる
     // compornentに値を渡す => props
     return <SignIn setName={setName} />;
